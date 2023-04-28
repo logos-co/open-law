f527f43b9e44a6f75475aaebc6e12024573ddb5d
SvyatoslavArtymovych <svyatoslav.artymovych@gmail.com>
2023-04-28 14:17:54 +0300

    hotfix: access to home pages

--- app/views/main.py
+++ app/views/main.py
@@ -1,11 +1,8 @@
 from flask import render_template, Blueprint
-from flask_login import login_required
-
 
 main_blueprint = Blueprint("main", __name__)
 
 
 @main_blueprint.route("/")
-@login_required
 def index():
     return render_template("index.html")
