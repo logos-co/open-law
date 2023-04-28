c9bf1816380b14dc96fedffbc752c64b53de93d1
SvyatoslavArtymovych <svyatoslav.artymovych@gmail.com>
2023-04-28 10:50:59 +0300

    BaseCollectionForm

--- app/forms/collection.py
+++ app/forms/collection.py
@@ -3,15 +3,14 @@ from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired, Length
 
 
-class CreateCollectionForm(FlaskForm):
+class BaseCollectionForm(FlaskForm):
     label = StringField("Label", [DataRequired(), Length(3, 256)])
     about = StringField("About")
 
+
+class CreateCollectionForm(BaseCollectionForm):
     submit = SubmitField("Create")
 
 
-class EditCollectionForm(FlaskForm):
-    label = StringField("Label", [Length(3, 256)])
-    about = StringField("About")
-
+class EditCollectionForm(BaseCollectionForm):
     submit = SubmitField("Edit")
