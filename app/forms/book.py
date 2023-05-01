0a6c5d3544b9d7736123f7e30ad13aa2959fb4cf
SvyatoslavArtymovych <svyatoslav.artymovych@gmail.com>
2023-05-01 18:17:46 +0300

    Edit book label

--- app/forms/book.py
+++ app/forms/book.py
@@ -3,6 +3,13 @@ from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired, Length
 
 
-class CreateBookForm(FlaskForm):
+class BaseBookForm(FlaskForm):
     label = StringField("Label", [DataRequired(), Length(6, 256)])
+
+
+class CreateBookForm(BaseBookForm):
     submit = SubmitField("Add new book")
+
+
+class EditBookForm(BaseBookForm):
+    submit = SubmitField("Edit book")
