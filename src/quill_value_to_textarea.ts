ff0afbf99e980e84ac94ea0f18f76d8407072c6c
Kostiantyn Stoliarskyi <saintkos117@gmail.com>
2023-05-05 10:35:15 +0300

    work

--- src/quill_value_to_textarea.ts
+++ src/quill_value_to_textarea.ts
@@ -5,15 +5,35 @@ const quill_value_to_textarea = (): undefined => {
   aboutInput.value = editorContent;
   return undefined;
 };
+const quill_interpretation_to_textarea = (): undefined => {
+  const interpretationTextInput: HTMLButtonElement = document.querySelector(
+    '#interpretation-text',
+  );
+  const qlInterpretationEditor: HTMLButtonElement = document.querySelector(
+    '.ql-interpretation-editor',
+  );
+  const editorContent = qlInterpretationEditor.innerHTML;
+  interpretationTextInput.value = editorContent;
+  return undefined;
+};
 
 export function initQuillValueToTextArea() {
   const qlEditor: HTMLButtonElement = document.querySelector('.ql-editor');
 
-  if (!qlEditor) {
-    return;
+  if (qlEditor) {
+    qlEditor.addEventListener('DOMSubtreeModified', async e => {
+      quill_value_to_textarea();
+    });
   }
 
-  qlEditor.addEventListener('DOMSubtreeModified', async e => {
-    quill_value_to_textarea();
-  });
+  //
+  const qlInterpretationEditor: HTMLButtonElement = document.querySelector(
+    '.ql-interpretation-editor',
+  );
+
+  if (!qlInterpretationEditor) {
+    qlInterpretationEditor.addEventListener('DOMSubtreeModified', async e => {
+      quill_interpretation_to_textarea();
+    });
+  }
 }
