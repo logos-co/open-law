76c1273d81c1cf12841cab2170602435a2fcda79
SvyatoslavArtymovych <svyatoslav.artymovych@gmail.com>
2023-05-05 19:51:55 +0300

    edit and delete interpretation UI

--- app/views/book.py
+++ app/views/book.py
@@ -1005,11 +1005,12 @@ def interpretation_edit(
             )
 
     redirect_url = url_for(
-        "book.interpretation_view",
+        "book.qa_view",
         book_id=book_id,
         collection_id=collection_id,
         sub_collection_id=sub_collection_id,
         section_id=section_id,
+        interpretation_id=interpretation_id,
     )
     section: m.Section = db.session.get(m.Section, section_id)
     if not section or section.is_deleted:
@@ -1123,4 +1124,89 @@ def interpretation_delete(
             "book.collection_view",
             book_id=book_id,
         )
+    )
+
+
+@bp.route(
+    "/<int:book_id>/<int:collection_id>/<int:section_id>/<int:interpretation_id>/preview",
+    methods=["GET", "POST"],
+)
+@bp.route(
+    (
+        "/<int:book_id>/<int:collection_id>/<int:sub_collection_id>/"
+        "<int:section_id>/<int:interpretation_id>/preview"
+    ),
+    methods=["GET", "POST"],
+)
+@login_required
+def qa_view(
+    book_id: int,
+    collection_id: int,
+    section_id: int,
+    interpretation_id: int,
+    sub_collection_id: int | None = None,
+):
+    book: m.Book = db.session.get(m.Book, book_id)
+    if not book or book.owner != current_user or book.is_deleted:
+        log(log.INFO, "User: [%s] is not owner of book: [%s]", current_user, book)
+        flash("You are not owner of this book!", "danger")
+        return redirect(url_for("book.my_books"))
+
+    collection: m.Collection = db.session.get(m.Collection, collection_id)
+    if not collection or collection.is_deleted:
+        log(log.WARNING, "Collection with id [%s] not found", collection_id)
+        flash("Collection not found", "danger")
+        return redirect(url_for("book.collection_view", book_id=book_id))
+
+    if sub_collection_id:
+        sub_collection: m.Collection = db.session.get(m.Collection, sub_collection_id)
+        if not sub_collection or sub_collection.is_deleted:
+            log(log.WARNING, "Sub_collection with id [%s] not found", sub_collection_id)
+            flash("SubCollection not found", "danger")
+            return redirect(
+                url_for(
+                    "book.sub_collection_view",
+                    book_id=book_id,
+                    collection_id=collection_id,
+                )
+            )
+
+    redirect_url = url_for(
+        "book.interpretation_view",
+        book_id=book_id,
+        collection_id=collection_id,
+        sub_collection_id=sub_collection_id,
+        section_id=section_id,
+    )
+    section: m.Section = db.session.get(m.Section, section_id)
+    if not section or section.is_deleted:
+        log(log.WARNING, "Section with id [%s] not found", section_id)
+        flash("Section not found", "danger")
+        return redirect(redirect_url)
+
+    interpretation: m.Interpretation = db.session.get(
+        m.Interpretation, interpretation_id
+    )
+    if not interpretation or interpretation.is_deleted:
+        log(log.WARNING, "Interpretation with id [%s] not found", interpretation_id)
+        flash("Interpretation not found", "danger")
+        return redirect(redirect_url)
+
+    breadcrumbs = create_breadcrumbs(
+        book_id=book_id,
+        collection_path=(
+            collection_id,
+            sub_collection_id,
+        ),
+        section_id=section_id,
+        interpretation_id=interpretation.id,
+    )
+    return render_template(
+        "book/qa_view.html",
+        book=book,
+        collection=collection,
+        sub_collection=sub_collection if sub_collection_id else None,
+        section=section,
+        interpretation=interpretation,
+        breadcrumbs=breadcrumbs,
     )
