eab7fc4b43be1d9e9d7330d1567447d9426a8695
Mykola <nchernov@gmail.com>
2023-05-09 16:59:51 +0300

    fix: separate build

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,11 @@
-import './styles.css';
 import {initBooks} from './books';
 import {initContributors} from './contributors';
 import {initWallet} from './wallet';
 import {initQuill} from './initQuill';
 import {initQuillValueToInput} from './quillValueToInput';
 
-document.addEventListener('DOMContentLoaded', () => {
-  initBooks();
-  initContributors();
-  initQuill();
-  initQuillValueToInput();
-  initWallet();
-});
+initBooks();
+initContributors();
+initQuill();
+initQuillValueToInput();
+initWallet();
