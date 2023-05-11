b611ad6b659bdb1662fbc0ea1344d19d2dfeeecc
Kostiantyn Stoliarskyi <saintkos117@gmail.com>
2023-05-11 10:16:13 +0300

    work

--- tests/test_users.py
+++ tests/test_users.py
@@ -7,44 +7,10 @@ from app import models as m, db
 from tests.utils import login
 
 
-def test_list(populate: FlaskClient):
-    login(populate)
-    DEFAULT_PAGE_SIZE = app.config["DEFAULT_PAGE_SIZE"]
-    response = populate.get("/user/")
-    assert response
-    assert response.status_code == 200
-    html = response.data.decode()
-    users = m.User.query.order_by(m.User.id).limit(11).all()
-    assert len(users) == 11
-    for user in users[:DEFAULT_PAGE_SIZE]:
-        assert user.username in html
-    assert users[10].username not in html
-
-    populate.application.config["PAGE_LINKS_NUMBER"] = 6
-    response = populate.get("/user/?page=6")
-    assert response
-    assert response.status_code == 200
-    html = response.data.decode()
-    assert "/user/?page=6" in html
-    assert "/user/?page=3" in html
-    assert "/user/?page=8" in html
-    assert "/user/?page=10" not in html
-    assert "/user/?page=2" not in html
-
-
 def test_create_admin(runner: FlaskCliRunner):
     res: Result = runner.invoke(args=["create-admin"])
     assert "admin created" in res.output
     assert m.User.query.filter_by(username=app.config["ADMIN_USERNAME"]).first()
-
-
-def test_delete_user(populate: FlaskClient):
-    login(populate)
-    users = m.User.query.all()
-    uc = len(users)
-    response = populate.delete("/user/delete/1")
-    assert m.User.query.count() < uc
-    assert response.status_code == 200
 
 
 def test_search_user(populate: FlaskClient, runner: FlaskCliRunner):
