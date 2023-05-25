377c3f00920bc8b384a22581bc91afde4a9837ff
Kostiantyn Stoliarskyi <saintkos117@gmail.com>
2023-05-25 17:22:56 +0300

    clean_up done

--- app/forms/interpretation.py
+++ app/forms/interpretation.py
@@ -1,6 +1,9 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, ValidationError
 from wtforms.validators import DataRequired
+
+from app.controllers import clean_html
+from app.logger import log
 
 
 class BaseInterpretationForm(FlaskForm):
@@ -11,6 +14,14 @@ class BaseInterpretationForm(FlaskForm):
 class CreateInterpretationForm(BaseInterpretationForm):
     section_id = StringField("Interpretation ID", [DataRequired()])
     submit = SubmitField("Create")
+
+    def validate_text(self, field):
+        text = clean_html(field.data)
+        text = text.replace("&nbsp;", "")
+        text = text.strip()
+        if len(text) < 1:
+            log(log.WARNING, "Can't submit empty interpretation")
+            raise ValidationError("You can't create interpretation with no text")
 
 
 class EditInterpretationForm(BaseInterpretationForm):
