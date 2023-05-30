252b2997ba7e6bd0cb3d753d1e490432955d995a
Kostiantyn Stoliarskyi <saintkos117@gmail.com>
2023-05-30 10:02:00 +0300

    refactored comments

--- app/views/book/comment.py
+++ app/views/book/comment.py
@@ -1,4 +1,4 @@
-from flask import flash, redirect, url_for, current_app
+from flask import flash, redirect, url_for, current_app, request
 from flask_login import login_required, current_user
 
 from app.controllers import (
@@ -14,63 +14,29 @@ from .bp import bp
 
 
 @bp.route(
-    "/<int:book_id>/<int:collection_id>/<int:section_id>/<int:interpretation_id>/create_comment",
-    methods=["POST"],
-)
-@bp.route(
-    (
-        "/<int:book_id>/<int:collection_id>/<int:sub_collection_id>/"
-        "<int:section_id>/<int:interpretation_id>/create_comment"
-    ),
+    "/<int:book_id>/<int:interpretation_id>/create_comment",
     methods=["POST"],
 )
 @login_required
 def create_comment(
     book_id: int,
-    collection_id: int,
-    section_id: int,
     interpretation_id: int,
-    sub_collection_id: int | None = None,
 ):
+    current_url = request.referrer
+    if not current_url:
+        current_url = url_for(
+            "book.collection_view",
+            book_id=book_id,
+        )
     book: m.Book = db.session.get(m.Book, book_id)
     if not book or book.is_deleted:
         log(log.INFO, "User: [%s] is not owner of book: [%s]", current_user, book)
         flash("You are not owner of this book!", "danger")
         return redirect(url_for("book.my_library"))
-
-    collection: m.Collection = db.session.get(m.Collection, collection_id)
-    if not collection or collection.is_deleted:
-        log(log.WARNING, "Collection with id [%s] not found", collection_id)
-        flash("Collection not found", "danger")
-        return redirect(url_for("book.collection_view", book_id=book_id))
-
-    sub_collection = None
-    if sub_collection_id:
-        sub_collection: m.Collection = db.session.get(m.Collection, sub_collection_id)
-        if not sub_collection or sub_collection.is_deleted:
-            log(log.WARNING, "Sub_collection with id [%s] not found", sub_collection_id)
-            flash("SubCollection not found", "danger")
-            return redirect(
-                url_for(
-                    "book.sub_collection_view",
-                    book_id=book_id,
-                    collection_id=collection_id,
-                )
-            )
-
     redirect_url = url_for(
-        "book.qa_view",
+        "book.collection_view",
         book_id=book_id,
-        collection_id=collection_id,
-        sub_collection_id=sub_collection_id,
-        section_id=section_id,
-        interpretation_id=interpretation_id,
     )
-    section: m.Section = db.session.get(m.Section, section_id)
-    if not section or section.is_deleted:
-        log(log.WARNING, "Section with id [%s] not found", section_id)
-        flash("Section not found", "danger")
-        return redirect(redirect_url)
 
     interpretation: m.Interpretation = db.session.get(
         m.Interpretation, interpretation_id
@@ -95,9 +61,8 @@ def create_comment(
 
         log(
             log.INFO,
-            "Create comment for interpretation [%s]. Section: [%s]",
+            "Create comment for interpretation [%s].",
             interpretation,
-            section,
         )
         comment.save()
 
@@ -105,7 +70,7 @@ def create_comment(
         set_comment_tags(comment, tags)
 
         flash("Success!", "success")
-        return redirect(redirect_url)
+        return redirect(current_url)
     else:
         log(log.ERROR, "Comment create errors: [%s]", form.errors)
         for field, errors in form.errors.items():
@@ -117,25 +82,21 @@ def create_comment(
 
 
 @bp.route(
-    "/<int:book_id>/<int:collection_id>/<int:section_id>/<int:interpretation_id>/comment_delete",
-    methods=["POST"],
-)
-@bp.route(
-    (
-        "/<int:book_id>/<int:collection_id>/<int:sub_collection_id>/"
-        "<int:section_id>/<int:interpretation_id>/comment_delete"
-    ),
+    "/<int:book_id>/<int:interpretation_id>/comment_delete",
     methods=["POST"],
 )
 @register_book_verify_route(bp.name)
 @login_required
 def comment_delete(
     book_id: int,
-    collection_id: int,
-    section_id: int,
     interpretation_id: int,
-    sub_collection_id: int | None = None,
 ):
+    current_url = request.referrer
+    if not current_url:
+        current_url = url_for(
+            "book.collection_view",
+            book_id=book_id,
+        )
     form = f.DeleteCommentForm()
     comment_id = form.comment_id.data
     comment: m.Comment = db.session.get(m.Comment, comment_id)
@@ -147,45 +108,32 @@ def comment_delete(
         comment.save()
 
         flash("Success!", "success")
-        return redirect(
-            url_for(
-                "book.qa_view",
-                book_id=book_id,
-                collection_id=collection_id,
-                sub_collection_id=sub_collection_id,
-                section_id=section_id,
-                interpretation_id=interpretation_id,
-            )
-        )
+        return redirect(current_url)
+    flash("Invalid id!", "danger")
     return redirect(
         url_for(
-            "book.sub_collection_view",
+            "book.collection_view",
             book_id=book_id,
-            collection_id=collection_id,
         )
     )
 
 
 @bp.route(
-    "/<int:book_id>/<int:collection_id>/<int:section_id>/<int:interpretation_id>/comment_edit",
-    methods=["POST"],
-)
-@bp.route(
-    (
-        "/<int:book_id>/<int:collection_id>/<int:sub_collection_id>/"
-        "<int:section_id>/<int:interpretation_id>/comment_edit"
-    ),
+    "/<int:book_id>/<int:interpretation_id>/comment_edit",
     methods=["POST"],
 )
 @register_book_verify_route(bp.name)
 @login_required
 def comment_edit(
     book_id: int,
-    collection_id: int,
-    section_id: int,
     interpretation_id: int,
-    sub_collection_id: int | None = None,
 ):
+    current_url = request.referrer
+    if not current_url:
+        current_url = url_for(
+            "book.collection_view",
+            book_id=book_id,
+        )
     form = f.EditCommentForm()
 
     if form.validate_on_submit():
@@ -202,20 +150,12 @@ def comment_edit(
         comment.save()
 
         flash("Success!", "success")
-        return redirect(
-            url_for(
-                "book.qa_view",
-                book_id=book_id,
-                collection_id=collection_id,
-                sub_collection_id=sub_collection_id,
-                section_id=section_id,
-                interpretation_id=interpretation_id,
-            )
-        )
+        return redirect(current_url)
+    flash("Invalid id!", "danger")
+
     return redirect(
         url_for(
-            "book.sub_collection_view",
+            "book.collection_view",
             book_id=book_id,
-            collection_id=collection_id,
         )
     )
