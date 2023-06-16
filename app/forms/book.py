aba25c662f9550a19e4a630938d63b4398cf03f9
SvyatoslavArtymovych <svyatoslav.artymovych@gmail.com>
2023-06-16 14:09:23 +0300

    remove book label validation on edit

--- app/forms/book.py
+++ app/forms/book.py
@@ -19,21 +19,3 @@ class CreateBookForm(BaseBookForm):
 class EditBookForm(BaseBookForm):
     book_id = StringField("User ID", [DataRequired()])
     submit = SubmitField("Edit book")
-
-    def validate_label(self, field):
-        label = field.data
-        book_id = self.book_id.data
-
-        existing_book: m.Book = (
-            m.Book.query.filter_by(
-                is_deleted=False,
-                label=label,
-            )
-            .filter(m.Book.id != book_id)
-            .first()
-        )
-        if existing_book:
-            log(
-                log.WARNING, "Book with label [%s] already exists: [%s]", label, book_id
-            )
-            raise ValidationError("Book label must be unique!")
